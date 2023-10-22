867ff72ac3b3831ff8fff5355a82ae1bc5e392d8
teo kurtagic <teo@sl-king.com>
2023-10-22 22:51:11 +0200

    added batch script for running tj.exe

---
+++ tjScript.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set /p "path=Enter the directory path: "
+
+for %%f in (%path%\*.java) do (
+    
+	set "scrFile=%%f"
+	set "testsFolder=%path%\tests\tests"
+	set "resultsFolder=%path%\tests\results"
+
+	if not exist !resultsFolder! (mkdir !resultsFolder!)
+		
+	tj.exe !scrFile! !testsFolder! !resultsFolder!
+)
+
+endlocal
