adcf5f12265914f05a2c82f32938efe726a76577
teo kurtagic <teo@sl-king.com>
2023-10-24 12:02:37 +0200

    fixed shit on laptop

--- tjScript.cmd
+++ tjScript.cmd
@@ -11,7 +11,9 @@ for %%f in (%path%\*.java) do (
 	set "resultsFolder=%path%\tests\results"
 
 	if not exist !resultsFolder! (mkdir !resultsFolder!)
-		
+	echo !scrFile!
+	echo !testsFolder!
+	echo !resultsFolder!
 	tj.exe !scrFile! !testsFolder! !resultsFolder!
 )
 
