1c7a39cfd3328a8b64dce7a0013ebffad02a0c95
teo kurtagic <teo@sl-king.com>
2023-11-08 18:00:33 +0100

    removed shit

--- reorganiseDirectory.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-for /F "delims=" %%a in ('dir D:\UNIVERSITY\BACHELORS\YEAR_1\SEMESTER_1\P1\DODATNE_NALOGE\03_tabele /s /b /o:n /a:d') do (
-   echo %%a
-	mkdir "%%a\tests\tests"
-	move "%%a\*.<in>" "%%a\tests\tests"
-	move "%%a\*.<out>" "%%a\tests\tests"
-
-)
