8c53ebb26072245aa1e1207359e927919e58d9f2
teo kurtagic <teo@sl-king.com>
2023-12-14 11:23:52 +0100

    fixed shit

---
+++ dn/dn-00/DN00_63230175.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DN00_63230175 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int b = sc.nextInt();
+		int p = sc.nextInt();
+		
+		System.out.println(b/p);
+		System.out.println(b%p);
+	}
+}
